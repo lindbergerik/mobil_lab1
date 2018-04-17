35e147418b995a332daceeafb5a368c9c37a0475
Erik Lindberg <lindbergerik@outlook.com>
2018-04-17 12:31:23 +0200

    finished app

--- interaction.js
+++ interaction.js
@@ -8,41 +8,40 @@ function zoomIn() {
 	map.setZoom(map.zoom + 1);
 }
 
-var startX = 0;
-var startY = 0;
+var coords = {lat: 0, lng: 0};
 
-function startPan(event) {
-	startX = event.clientX;     // Get the horizontal coordinate
-	startY = event.clientY;     // Get the vertical coordinate
-	console.log("X coords: " + startX + ", Y coords: " + startY);
+function panLeft() {
+  coords = map.getCenter();
+  map.setCenter({lat: coords.lat(), lng: coords.lng()-0.001});
 }
 
-function stopPan(event) {
-	var distX = startX - event.clientX;
-	var distY = startY - event.clientY;
-	map.panBy(distX, distY);	//pannar automatiskt genom skillnad i X och Y värdena
-	console.log("X coords: " + distX + ", Y coords: " + distY);
+function panUp() {
+  coords = map.getCenter();
+  map.setCenter({lat: coords.lat()+0.001, lng: coords.lng()});
 }
 
-function toggleFullScreen() {
-  var doc = window.document;
-  var docEl = doc.documentElement;
-
-  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-    requestFullScreen.call(docEl);
-  }
-  else {
-    cancelFullScreen.call(doc);
-  }
+function panDown() {
+  coords = map.getCenter();
+  map.setCenter({lat: coords.lat()-0.001, lng: coords.lng()});
 }
 
-// var coordinates = {lat: 0, lng: 0};
-// function placeMarker(event) {
-// 	var coordinates = {lat: event.clientX, lng: event.clientY};
-// 	map.marker.position=coordinates;
-// 	//map.marker.setMap(map);
-// 	//PLACE MARKER
+function panRight() {
+  coords = map.getCenter();
+  map.setCenter({lat: coords.lat(), lng: coords.lng()+0.001});
+}
+
+// Panning by drag
+// var startX = 0;
+// var startY = 0;
+// function startPan(event) {
+// 	startX = event.clientX;     // Get the horizontal coordinate
+// 	startY = event.clientY;     // Get the vertical coordinate
+// 	console.log("X coords: " + startX + ", Y coords: " + startY);
 // }
+// function stopPan(event) {
+// 	var distX = startX - event.clientX;
+// 	var distY = startY - event.clientY;
+// 	map.panBy(distX, distY);	//pannar automatiskt genom skillnad i X och Y värdena
+// 	console.log("X coords: " + distX + ", Y coords: " + distY);
+// }
+
--- map.js
+++ map.js
@@ -1,4 +1,5 @@
-var map;
+
+var map, infowindow;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 59.336559, lng: 18.062660},
@@ -10,9 +11,8 @@ function initMap() {
 		draggable: false
 	});
 
-	var coordinates = {lat: 59.336559, lng: 18.062660};
 	this.marker1 = new google.maps.Marker( {
-		position: coordinates,
+		position: {lat: 59.336559, lng: 18.062660},
 		map: map,
 		draggable: true,
 		animation: google.maps.Animation.DROP,
@@ -25,4 +25,73 @@ function initMap() {
 		animation: google.maps.Animation.BOUNCE,
 		title: 'HOLA'
 	});
+
+	var icon1 = {
+	    url: "https://github.com/lindbergerik/lindbergerik.github.io/blob/master/erik.jpg?raw=true", // url
+	    scaledSize: new google.maps.Size(50, 50), // scaled size
+	    origin: new google.maps.Point(0,0), // origin
+	    anchor: new google.maps.Point(0, 0) // anchor
+	};
+	var coordsErik = {lat: 59.312154, lng: 18.079739};
+	this.marker3 = new google.maps.Marker( {
+		position: coordsErik,
+		map: map,
+		icon: icon1,
+		title: 'Hannas!'
+	});
+
+	var icon2 = {
+	    url: "https://github.com/lindbergerik/lindbergerik.github.io/blob/master/sebbe.jpg?raw=true", // url
+	    scaledSize: new google.maps.Size(50, 50), // scaled size
+	    origin: new google.maps.Point(0,0), // origin
+	    anchor: new google.maps.Point(0, 0) // anchor
+	};
+	var coordsSebbe = {lat: 59.341122, lng: 18.058018};
+	this.marker4 = new google.maps.Marker( {
+		position: coordsSebbe,
+		map: map,
+		icon: icon2,
+		title: 'Hirschen!'
+	});
+
+}
+
+function geoLocation() {
+	//GEOLOCATION
+	infoWindow = new google.maps.InfoWindow;
+	// Try HTML5 geolocation.
+	if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(function(position) {
+	    	var pos = {
+	        	lat: position.coords.latitude,
+	        	lng: position.coords.longitude
+	    	};
+
+	        infoWindow.setPosition(pos);
+	        infoWindow.setContent('Location found.');
+	        infoWindow.open(map);
+	        map.setCenter(pos);
+	    }, function() {
+	    	handleLocationError(true, infoWindow, map.getCenter());
+		});
+	} else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+function goErik() {
+	map.setCenter({lat: 59.312154, lng: 18.079739});
+}
+
+function goSebbe() {
+	map.setCenter({lat: 59.341122, lng: 18.058018});
 }
